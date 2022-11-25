2cee53d98430259f2bb65413a240269364a14475
Lucas Rocha Andrade <62523002+Karnameron@users.noreply.github.com>
2022-11-24 22:22:27 -0300

    Add files via upload

---
+++ src/main/java/com/mycompany/pdfimporter/FileUploadView.java
@@ -0,0 +1,112 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.pdfimporter;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+import org.primefaces.PrimeFaces;
+import org.primefaces.event.FileUploadEvent;
+import org.primefaces.event.FilesUploadEvent;
+import org.primefaces.model.file.UploadedFile;
+import org.primefaces.model.file.UploadedFiles;
+import org.primefaces.shaded.commons.io.FilenameUtils;
+import org.primefaces.shaded.commons.io.IOUtils;
+import org.primefaces.util.EscapeUtils;
+
+/**
+ *
+ * @author Lucas Rocha Andrade
+ */
+@Named
+@RequestScoped
+public class FileUploadView {
+
+    private UploadedFile file;
+    private UploadedFiles files;
+    private String dropZoneText = "Drop zone p:inputTextarea demo.";
+
+    public void save() throws IOException {
+        if (file != null) {
+            FacesMessage message = new FacesMessage("Successful", file.getFileName() + " is uploaded.");
+            FacesContext.getCurrentInstance().addMessage(null, message);
+
+        }
+        String filename = FilenameUtils.getName(file.getFileName());
+        InputStream input = file.getInputStream();
+        OutputStream output = new FileOutputStream(new File("C:\\Users\\Lucas Rocha Andrade\\Documents\\NetBeansProjects\\PDFImporter\\src\\main\\webapp\\documentos", filename));
+
+        try {
+            IOUtils.copy(input, output);
+        } finally {
+            IOUtils.closeQuietly(input);
+            IOUtils.closeQuietly(output);
+        }
+    }
+
+    public void uploadMultiple() {
+        if (files != null) {
+            for (UploadedFile f : files.getFiles()) {
+                FacesMessage message = new FacesMessage("Successful", f.getFileName() + " is uploaded.");
+                FacesContext.getCurrentInstance().addMessage(null, message);
+            }
+        }
+    }
+
+    public void handleFileUpload(FileUploadEvent event) {
+        FacesMessage message = new FacesMessage("Successful", event.getFile().getFileName() + " is uploaded.");
+        FacesContext.getCurrentInstance().addMessage(null, message);
+    }
+
+    public void handleFileUploadTextarea(FileUploadEvent event) {
+        String jsVal = "PF('textarea').jq.val";
+        String fileName = EscapeUtils.forJavaScript(event.getFile().getFileName());
+        PrimeFaces.current().executeScript(jsVal + "(" + jsVal + "() + '\\n\\n" + fileName + " uploaded.')");
+    }
+
+    public void handleFilesUpload(FilesUploadEvent event) {
+        for (UploadedFile f : event.getFiles().getFiles()) {
+            FacesMessage message = new FacesMessage("Successful", f.getFileName() + " is uploaded.");
+            FacesContext.getCurrentInstance().addMessage(null, message);
+        }
+    }
+    
+    public String[] returnArchives() {
+        File arquivo = new File("C:\\Users\\Lucas Rocha Andrade\\Documents\\NetBeansProjects\\PDFImporter\\src\\main\\webapp\\documentos");
+
+        return arquivo.list();
+    }
+
+    public UploadedFile getFile() {
+        return file;
+    }
+
+    public void setFile(UploadedFile file) {
+        this.file = file;
+    }
+
+    public UploadedFiles getFiles() {
+        return files;
+    }
+
+    public void setFiles(UploadedFiles files) {
+        this.files = files;
+    }
+
+    public String getDropZoneText() {
+        return dropZoneText;
+    }
+
+    public void setDropZoneText(String dropZoneText) {
+        this.dropZoneText = dropZoneText;
+    }
+
+}
